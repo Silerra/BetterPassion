b501213e5793d27b1d9ad05ce426adddccc72f8e
Silerra <silerra@live.com>
2025-02-03 21:12:02 +0100

    Try something corrections

--- src/PatchWorkTab.cs
+++ src/PatchWorkTab.cs
@@ -5,19 +5,34 @@ using Verse;
 
 namespace BetterPassionIcons
 {
-    [HarmonyPatch(typeof(PawnColumnWorker_WorkPriority), "DoCell")]
+    [HarmonyPatch(typeof(PawnColumnWorker_WorkPriority), nameof(PawnColumnWorker_WorkPriority.DoCell))]
     public static class Patch_WorkPriorityIconSize
     {
-        static void Postfix(Rect rect, Pawn pawn, PawnTable table)
+        static void Postfix(Rect rect, Pawn pawn, PawnColumnWorker_WorkPriority __instance)
         {
-            // Vergrößere das Symbol auf 20x20 Pixel
-            Rect passionRect = new Rect(rect.x + 4f, rect.y + 4f, 20f, 20f);
-            Passion passion = pawn.skills?.GetSkill(table.def.workType.relevantSkill)?.passion ?? Passion.None;
-            
-            if (passion == Passion.Major)
-                GUI.DrawTexture(passionRect, MyTextures.MajorPassion); // Deine benutzerdefinierte Textur
-            else if (passion == Passion.Minor)
-                GUI.DrawTexture(passionRect, MyTextures.MinorPassion);
+            if (pawn?.skills == null)
+                return;
+
+            // Hole die WorkTypeDef aus der PawnColumnDef
+            WorkTypeDef workTypeDef = __instance.def.workType;
+            if (workTypeDef?.relevantSkill == null)
+                return;
+
+            // Hole die Passion für die relevante Skill-Def
+            Passion passion = pawn.skills.GetSkill(workTypeDef.relevantSkill).passion;
+            if (passion == Passion.None)
+                return;
+
+            // Lade die benutzerdefinierten Texturen aus den XML-Defs
+            PassionDef passionDef = (passion == Passion.Major) 
+                ? DefDatabase<PassionDef>.GetNamed("MajorPassion") 
+                : DefDatabase<PassionDef>.GetNamed("MinorPassion");
+
+            Texture2D icon = ContentFinder<Texture2D>.Get(passionDef.iconPath);
+
+            // Zeichne das Symbol (20x20 Pixel)
+            Rect iconRect = new Rect(rect.x + 2f, rect.y + 2f, 20f, 20f);
+            GUI.DrawTexture(iconRect, icon);
         }
     }
 }
